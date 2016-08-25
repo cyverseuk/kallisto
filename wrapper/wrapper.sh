4b3209c337ac429ecbd5b200b3960076c5a0c1e3
Alice Minotto <i.m.e@hotmail.it>
2016-08-25 10:49:26 +0100

    Update wrapper.sh

--- wrapper/wrapper.sh
+++ wrapper/wrapper.sh
@@ -5,10 +5,10 @@ INPUTS="${FASTA}, ${fasta_index}, ${FASTQ}"
 echo ${ARGS}
 echo ${INPUTS}
 
-INDEX=${index}
-FASTA_INDEX=${fasta_index}
-IN_NAME=${in_name}
-H5DUMP=${h5dump}
+INDEX="${index}"
+FASTA_INDEX="${fasta_index}"
+IN_NAME="${in_name}"
+H5DUMP="${h5dump}"
 SINGLE="${single}"
 FRAG_LEN="${frag_len}"
 SD="${sd}"
@@ -24,39 +24,51 @@ if [ -n "${SINGLE}" ]
 fi
 
 CMDLINEARG=""
+#echo "starting if part";
+#echo ${index};
 if [ -z "${INDEX}" ]
   then
+    #echo 1;
     if [ -z "${FASTA_INDEX}" ]
       then
+        #echo 2;
         echo "A FASTA index is required. Run 'index' or provide a File"
         exit 1;
     else
+      #echo 2bis;
       ${IN_NAME}=${FASTA_INDEX}
     fi
 else
+  #echo 1bis;
   CMDLINEARG+="kallisto ${INDEX} ${IN_NAME} ${kmer} ${mu} ${fasta}; "
 fi
-CMDLINEARG+="kallisto ${alg} ${IN_NAME} --output-dir output ${single} ${frag_len} ${sd} ${pseudobam} "
+CMDLINEARG+="kallisto ${alg} ${IN_NAME} --output-dir . ${single} ${frag_len} ${sd} ${pseudobam} "
 if [ "${alg}" == "quant" ]
   then
+    #echo 3;
     CMDLINEARG+="${bias} ${bs} ${seed} ${plaintext} ${frs} ${rfs} "
 else
+  #echo 3bis;
   CMDLINEARG+="${umi} "
 fi
 CMDLINEARG+="${fastq}; "
+#echo ${H5DUMP}
 if [ -n "${H5DUMP}" ]
   then
+    #echo 4;
     if [ "${alg}" != "quant" ]
       then
+        #echo 5;
         echo "h5dump may be used just after 'quant' mode"
         exit 1;
     else
+      #echo 5bis;
       echo "WARNING: h5dump will overwrite .tsv file"
-      CMDLINEARG+="kallisto ${H5DUMP} --output-dir output output/abundance.h5;"
+      CMDLINEARG+="kallisto ${H5DUMP} --output-dir . output/abundance.h5 "
     fi
 fi
 
-echo ${CMDLINEARG};
+echo "${CMDLINEARG}";
 
 echo  universe                = docker >> lib/condorSubmitEdit.htc
 echo docker_image            =  cyverseuk/kallisto:v0.43.0 >> lib/condorSubmitEdit.htc ######
