c6426471996abdc1aa0f8885d523b27b09c043b7
Alice Minotto <i.m.e@hotmail.it>
2016-10-25 17:16:26 +0100

    Update wrapper.sh
    fixed for agave case insensitivity

--- wrapper/wrapper.sh
+++ wrapper/wrapper.sh
@@ -3,26 +3,28 @@
 rmthis=`ls`
 echo ${rmthis}
 
-ARGS=" ${index} ${alg} ${h5dump} ${in_name} ${kmer} ${mu} --output-dir output ${bias} ${bs} ${seed} ${plaintext} ${single} ${frs} ${rfs} ${frag_len} ${sd} ${pseudobam} ${umi}"
-FASTA=`echo ${fasta} | sed -e 's/ /, /g'`
-FASTQ=`echo ${fastq} | sed -e 's/ /, /g'`
-INPUTS="${FASTA}, ${fasta_index}, ${FASTQ}"
-echo "arguments are "${ARGS}
-echo "inputs are "${INPUTS}
+ARGSU=" ${index} ${alg} ${h5dump} ${in_name} ${kmer} ${mu} --output-dir output ${bias} ${bs} ${seed} ${plaintext} ${single} ${frs} ${rfs} ${frag_len} ${sd} ${pseudobam} ${umi}"
+FASTAU=`echo ${fasta} | sed -e 's/ /, /g'`
+FASTQU=`echo ${fastq} | sed -e 's/ /, /g'`
+INPUTSU="${FASTAU}, ${fasta_index}, ${FASTQU}"
+echo "FASTQU is " "${FASTQU}"
+echo "fastq is " "${fastq}"
+echo "arguments are "${ARGSU}
+echo "inputs are "${INPUTSU}
 
-INDEX="${index}"
-FASTA_INDEX="${fasta_index}"
-IN_NAME="${in_name}"
-H5DUMP="${h5dump}"
-SINGLE="${single}"
-FRAG_LEN="${frag_len}"
-SD="${sd}"
+INDEXU="${index}"
+FASTA_INDEXU="${fasta_index}"
+IN_NAMEU="${in_name}"
+H5DUMPU="${h5dump}"
+SINGLEU="${single}"
+FRAG_LENU="${frag_len}"
+SDU="${sd}"
 #echo ${output}
 
 
-if [ -n "${SINGLE}" ]
+if [ -n "${SINGLEU}" ]
   then
-    if [ -z "${FRAG_LEN}" ] || [ -z "${SD}" ]
+    if [ -z "${FRAG_LENU}" ] || [ -z "${SDU}" ]
       then
         echo "for  single read fragment length and standard deviation must be provided"
         exit 1;
@@ -32,23 +34,23 @@ fi
 CMDLINEARG=""
 #echo "starting if part";
 #echo ${index};
-if [ -z "${INDEX}" ]
+if [ -z "${INDEXU}" ]
   then
     #echo 1;
-    if [ -z "${FASTA_INDEX}" ]
+    if [ -z "${FASTA_INDEXU}" ]
       then
         #echo 2;
         echo "A FASTA index is required. Run 'index' or provide a File"
         exit 1;
     else
       #echo 2bis;
-      ${IN_NAME}=${FASTA_INDEX}
+      ${IN_NAMEU}=${FASTA_INDEXU}
     fi
 else
   #echo 1bis;
-  CMDLINEARG+="kallisto ${INDEX} ${IN_NAME} ${kmer} ${mu} ${fasta}; "
+  CMDLINEARG+="kallisto ${INDEXU} ${IN_NAMEU} ${kmer} ${mu} ${fasta}; "
 fi
-CMDLINEARG+="kallisto ${alg} ${IN_NAME} --output-dir output ${single} ${frag_len} ${sd} ${pseudobam} "
+CMDLINEARG+="kallisto ${alg} ${IN_NAMEU} --output-dir output ${single} ${frag_len} ${sd} ${pseudobam} "
 if [ "${alg}" == "quant" ]
   then
     #echo 3;
@@ -59,7 +61,7 @@ CMDLINEARG+="${umi} "
 fi
 CMDLINEARG+="${fastq}; "
 #echo ${H5DUMP}
-if [ -n "${H5DUMP}" ]
+if [ -n "${H5DUMPU}" ]
   then
     #echo 4;
     if [ "${alg}" != "quant" ]
@@ -70,7 +72,7 @@ if [ -n "${H5DUMP}" ]
     else
       #echo 5bis;
       echo "WARNING: h5dump will overwrite .tsv file"
-      CMDLINEARG+="kallisto ${H5DUMP} --output-dir output output/abundance.h5 "
+      CMDLINEARG+="kallisto ${H5DUMPU} --output-dir output output/abundance.h5 "
     fi
 fi
 
@@ -82,7 +84,7 @@ echo  universe                = docker >> lib/condorSubmitEdit.htc
 echo docker_image            =  cyverseuk/kallisto:v0.43.0 >> lib/condorSubmitEdit.htc ######
 echo executable               =  ./launch.sh >> lib/condorSubmitEdit.htc #####
 echo arguments                          = ${CMDLINEARG} >> lib/condorSubmitEdit.htc
-echo transfer_input_files = ${INPUTS}, launch.sh >> lib/condorSubmitEdit.htc
+echo transfer_input_files = ${INPUTSU}, launch.sh >> lib/condorSubmitEdit.htc
 echo transfer_output_files = output >> lib/condorSubmitEdit.htc
 cat /mnt/data/rosysnake/lib/condorSubmit.htc >> lib/condorSubmitEdit.htc
 
